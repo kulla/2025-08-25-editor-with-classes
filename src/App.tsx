2cec542c9ae1300de1f3c0498afef91f30aa901c
Stephan Kulla <git.mail@kulla.me>
2025-09-02 22:03:01 +0200

    refactor(restructure): Add editor-state.ts

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { invariant } from 'es-toolkit'
 import { useEffect, useRef, useSyncExternalStore } from 'react'
 import { DebugPanel } from './components/debug-panel'
 import type { EntryValue, JSONValue, NodeType } from './nodes/types'
+import { EditorState } from './state/editor-state'
 import type { Entry, Key, ReadonlyState, WriteableState } from './state/types'
-import { getSingletonYDoc } from './state/ydoc'
 
 export default function App() {
   const { state } = useEditorState()
@@ -186,79 +186,7 @@ function useEditorState() {
   )
 }
 
-class EditorState implements ReadonlyState {
-  private lastKey = -1
-  private readonly ydoc
-  private readonly state
-  private readonly entries
-
-  constructor(ydoc = getSingletonYDoc()) {
-    this.ydoc = ydoc
-    this.state = this.ydoc.getMap('state')
-    this.entries = this.ydoc.getMap<Entry>('entries')
-  }
-
-  get<T extends NodeType>(key: Key<T>): Entry<T> {
-    const entry = this.entries.get(key) as Entry<T> | undefined
-
-    invariant(entry != null, `Node with key ${key} does not exist`)
-
-    return entry
-  }
-
-  getEntries(): [string, Entry][] {
-    return Array.from(this.entries.entries())
-  }
-
-  has(key: Key): boolean {
-    return this.entries.has(key)
-  }
-
-  addUpdateListener(listener: () => void) {
-    this.ydoc.on('update', listener)
-  }
-
-  removeUpdateListener(listener: () => void) {
-    this.ydoc.off('update', listener)
-  }
-
-  update(updateFn: (t: Transaction) => void) {
-    this.ydoc.transact(() => {
-      updateFn(
-        new Transaction(
-          (key) => this.get(key),
-          (key, entry) => this.set(key, entry),
-          (type) => this.generateKey(type),
-        ),
-      )
-      this.incrementUpdateCount()
-    })
-  }
-
-  get updateCount(): number {
-    const updateCount = this.state.get('updateCount') ?? 0
-
-    invariant(typeof updateCount === 'number', 'updateCounter must be a number')
-
-    return updateCount
-  }
-
-  private incrementUpdateCount() {
-    this.state.set('updateCount', this.updateCount + 1)
-  }
-
-  private set<T extends NodeType>(key: Key<T>, entry: Entry<T>) {
-    this.entries.set(key, entry)
-  }
-
-  private generateKey<T extends NodeType>(type: T): Key<T> {
-    this.lastKey += 1
-
-    return `${type}:${this.lastKey}`
-  }
-}
-
-class Transaction implements WriteableState {
+export class Transaction implements WriteableState {
   constructor(
     public readonly get: <T extends NodeType>(key: Key<T>) => Entry<T>,
     private readonly set: <T extends NodeType>(
