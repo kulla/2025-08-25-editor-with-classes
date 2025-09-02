3d7a4c98d18b2f6cf3307c3bdc0671186e6a786b
Stephan Kulla <git.mail@kulla.me>
2025-09-02 21:48:42 +0200

    doc: Document nodes/types.ts

--- src/nodes/types.ts
+++ src/nodes/types.ts
@@ -1,5 +1,15 @@
+/**
+ * Union of all possible node types (NodeMap is defined in each node file
+ * via `declare module`)
+ */
 export type NodeType = keyof NodeMap
 
+/**
+ * The external JSON representation of a node
+ */
 export type JSONValue<T extends NodeType> = NodeMap[T]['jsonValue']
 
+/**
+ * The internal (flat) representation of a node's value
+ */
 export type EntryValue<T extends NodeType> = NodeMap[T]['entryValue']
